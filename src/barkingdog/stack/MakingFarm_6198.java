137b970c629e09d5f6e2df5b0b42a08cc1fd773b
GoRyeoJin <sooon9254@naver.com>
2024-01-05 22:14:18 +0900

    Algorithm Stack 문제 풀이

---
+++ src/barkingdog/stack/MakingFarm_6198.java
@@ -0,0 +1,28 @@
+package barkingdog.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class MakingFarm_6198 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<N; i++) {
+            int h = Integer.parseInt(br.readLine());
+
+            while (!stack.isEmpty() && stack.peek() <= h) {
+                stack.pop();
+            }
+            stack.push(h);
+            sum += stack.size() - 1;
+            // 0 1 1 2 0 1
+        }
+    }
+}
