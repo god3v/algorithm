ce049be8fc0baeedf8bd391bef66b5d7c091aa70
고려진 <sooon9254@naver.com>
2024-12-24 00:11:44 +0900

    [Bronze IV] Title: 숫자의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -0,0 +1,15 @@
+# 1. N값 입력 받기
+N = int(input())
+
+# 2. 길이 N의 숫자를  sNum에 저장하기
+sNum = input()
+
+# 3. int형 변수 sum 선언하기
+result = 0
+
+# 4. sNum 의 길이만큼 반복문을 돌아 각 자릿값을 숫자로 변환하여 sum에 누적하기
+for i in sNum:
+    result += int(i)
+
+# 5. sum 출력하기
+print(result)
