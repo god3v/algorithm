aca810ac7504432fbfa6f74c8d5fd52ff0ac6287
goryeojin <sooon9254@naver.com>
2026-08-01 19:59:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.10 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i < s || i >= overwrite_string.length() + s)
+                sb.append(my_string.charAt(i));
+            else
+                sb.append(overwrite_string.charAt(i - s));
+        }
+        return sb.toString();
+    }
+}
